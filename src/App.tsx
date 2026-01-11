1d3878cb7d15f8984fc7e14cd92f44a79356ed01
long.pham <phamquanglong.work@gmail.com>
2026-01-11 20:51:39 +0700

    Modify app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import AppLayout from './ui/AppLayout';
+import Box from './ui/Box';
 import Header from './ui/Header';
 
 function App() {
   return (
-    <div className="bg-background-900 h-screen p-3.5">
-      <AppLayout>
-        <Header></Header>
-      </AppLayout>
+    <div className="bg-background-900 flex h-screen flex-col gap-4 p-5">
+      <Header></Header>
+      <main className="flex h-full justify-center gap-4">
+        <Box></Box>
+        <Box></Box>
+      </main>
     </div>
   );
 }
--- src/features/movies/SearchBox.tsx
+++ src/features/movies/SearchBox.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function SearchBox() {
-  return <input className="" placeholder="Search movies..." />;
+  return (
+    <input
+      className="bg-primary-light w-xs min-w-[150px] rounded-lg px-3 py-2 text-lg duration-150 ease-in-out focus:-translate-y-0.5 focus:shadow-lg/10 focus:outline-hidden"
+      placeholder="Search movies..."
+    />
+  );
 }
