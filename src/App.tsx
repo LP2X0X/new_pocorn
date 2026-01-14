adc6a6f1bf26341b38314ef9984c0b3642419a77
long.pham <phamquanglong.work@gmail.com>
2026-01-14 10:38:14 +0700

    Add StatusMessage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Box from './ui/Box';
 import Header from './ui/Header';
+import StatusMessage from './ui/StatusMessage';
 
 function App() {
   return (
     <div className="bg-background-900 flex h-screen flex-col gap-4 p-5">
       <Header></Header>
       <main className="flex h-full justify-center gap-4">
-        <Box></Box>
+        <Box>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <StatusMessage />
+          </div>
+        </Box>
         <Box></Box>
       </main>
     </div>
---
+++ src/ui/StatusMessage.tsx
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux';
+import type { RootState } from '../store';
+
+export default function StatusMessage() {
+  const { status, error } = useSelector((state: RootState) => state.movies);
+
+  if (status === 'loading') {
+    return (
+      <i className="text-text text-2xl font-semibold uppercase">Loading...</i>
+    );
+  }
+
+  if (status === 'error') {
+    if (error === 'Movies not found') {
+      return (
+        <p className="text-text text-2xl font-semibold">
+          &#9940;️ Movies not found
+        </p>
+      );
+    } else {
+      return (
+        <p className="text-text text-2xl font-semibold">&#9940; {error}</p>
+      );
+    }
+  }
+
+  return;
+}
